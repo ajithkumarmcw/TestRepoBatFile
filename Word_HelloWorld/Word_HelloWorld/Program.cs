fff5cc80a79004ff77e7afc0c56c9e40f343682a
ajithkumarv <ajithkumar@multicorewareinc.com>
2021-12-05 23:04:00 +0530

    Add Word_HelloWorld

---
+++ Word_HelloWorld/Word_HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Word;
+
+namespace Word_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hellow i am ajith");
+        }
+    }
+}
